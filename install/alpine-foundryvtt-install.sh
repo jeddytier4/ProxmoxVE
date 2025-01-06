feb385ad1b4d6870f8a5bb54c99961e5d51bca48
Joshua Eddy <josh.eddy@distributedinc.com>
2025-01-06 16:11:11 -0500

    updated install

--- install/alpine-foundryvtt-install.sh
+++ install/alpine-foundryvtt-install.sh
@@ -35,43 +35,46 @@ else
 fi
 msg_info "Downloading FoundryVTT"
 wget -q -O "$FOUNDRY_APP_DIR/foundryvtt.zip" "$VTT_TEMP_URL"
+msg_ok "Downloading FoundryVTT"
 msg_info "Expanding FoundryVTT"
 unzip -qq -o "$FOUNDRY_APP_DIR/foundryvtt.zip" -d "$FOUNDRY_APP_DIR"
 chown -R foundry: "$FOUNDRY_APP_DIR" "$FOUNDRY_DATA_DIR"
-npm install pm2@latest -g
+msg_ok "Expanding FoundryVTT"
+npm install pm2@latest -g --no-fund --silent
 
-pm2 start "$FOUNDRY_APP_DIR/resources/app/main.js" --name foundry --user foundry -- --dataPath="$FOUNDRY_DATA_DIR"
+msg_info "Starting FoundryVTT"
+pm2 start "$FOUNDRY_APP_DIR/resources/app/main.js" --name foundry --user foundry -- --dataPath="$FOUNDRY_DATA_DIR" --silent
 
 # Allow PM2 to start at boot
-pm2 startup
-pm2 save
+pm2 startup --silent
+pm2 save --silent
 
-  cat > "$FOUNDRY_DATA_DIR/Config/options.json" <<EOF
-{
-  "port": 30000,
-  "upnp": true,
-  "fullscreen": false,
-  "hostname": "${FOUNDRY_HOSTNAME}",
-  "localHostname": null,
-  "routePrefix": null,
-  "sslCert": null,
-  "sslKey": null,
-  "awsConfig": null,
-  "dataPath": "${APP}",
-  "passwordSalt": null,
-  "proxySSL": true,
-  "proxyPort": 443,
-  "serviceConfig": null,
-  "updateChannel": "stable",
-  "language": "en.core",
-  "upnpLeaseDuration": null,
-  "compressStatic": true,
-  "world": null
-}
-EOF
+#   cat > "$FOUNDRY_DATA_DIR/Config/options.json" <<EOF
+# {
+#   "port": 30000,
+#   "upnp": true,
+#   "fullscreen": false,
+#   "hostname": "${FOUNDRY_HOSTNAME}",
+#   "localHostname": null,
+#   "routePrefix": null,
+#   "sslCert": null,
+#   "sslKey": null,
+#   "awsConfig": null,
+#   "dataPath": "${APP}",
+#   "passwordSalt": null,
+#   "proxySSL": true,
+#   "proxyPort": 443,
+#   "serviceConfig": null,
+#   "updateChannel": "stable",
+#   "language": "en.core",
+#   "upnpLeaseDuration": null,
+#   "compressStatic": true,
+#   "world": null
+# }
+# EOF
 
   # Restart Foundry to take proxying into account
-  pm2 restart foundry
+  pm2 restart foundry --silent
 
 msg_ok "FoundryVTT Installed"
 
